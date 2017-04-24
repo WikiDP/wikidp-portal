5f0da62a01a662f15a502cde06c30af6d97d4741
Carl Wilson <carl@openplanetsfoundation.org>
2017-04-24 18:33:22 +0100

    FEAT - Wikidata Integrator - removed all pywikibot code; - added basic wikidataintegrator example; - credentials config through environment variables; - replaced all routes with simple query proof of concept home page. - hacked queries as string constants for now; and - added basic README with quick start instructions.

--- wikidp/portal_routes.py
+++ wikidp/portal_routes.py
@@ -10,41 +10,14 @@
 # about the terms of this license.
 #
 """ Flask application routes for Wikidata portal. """
-import re
 
-from flask import flash, render_template, request
-import wtforms
-from wikidp.wikidata import print_page
 from wikidp import APP
+from wikidp.wikidata import test_query
 
-class ReusableForm(wtforms.Form):
-    """Valdiators for application forms."""
-    name = wtforms.TextField('Name:', validators=[wtforms.validators.required()])
-
-
-@APP.route("/", methods=['GET', 'POST'])
+@APP.route("/")
 def home():
-    """Default landing page."""
-    form = ReusableForm(request.form)
-
-    print(form.errors)
-    if request.method == 'POST':
-        name = request.form['name']
-        print(name)
-        item_list = [x.upper() for x in re.findall("Q\d+|q\d+", name)]
-        if form.validate():
-            # Save the comment here.
-            for q_id in item_list:
-                flash(q_id + ':  \n' + print_page(q_id))
-        else:
-            flash('All the form fields are required. ')
-    return render_template('home.html', form=form)
-
-@APP.route("/search/<string:name>/")
-def search(name):
-    """ search route. """
-    # text = wikidata_test()
-    return render_template('temp.html', name=name, output=print_page(name))
+    test_query()
+    return "Hello"
 
 if __name__ == "__main__":
     APP.run()
--- wikidp/wikidata.py
+++ wikidp/wikidata.py
@@ -9,59 +9,12 @@
 # License, Version 3. See the text file "COPYING" for further details
 # about the terms of this license.
 #
-""" Some basic Wikidata examples to go with our PywikiBot """
-import os.path
-import pywikibot
-from pywikibot import pagegenerators as pg
+"""Some basic Wikidata examples using WikidataIntegrator"""
+from wikidataintegrator import wdi_core, wdi_login
+from wikidp import APP
+from wikidp.queries import QUERIES
 
-SITE = pywikibot.Site("wikidata", "wikidata")
-REPO = SITE.data_repository()
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-
-def wikidata_test():
-    """ Simple Proof of Concept test that prints an item. """
-    # item = pywikibot.ItemPage(REPO, "Q26085352")
-    item = pywikibot.ItemPage(REPO, "Q178051")
-    item_dict = item.get() #Get the item dictionary
-    clm_dict = item_dict["claims"] # Get the claim dictionary
-    return str(clm_dict)
-
-def printer(dictionary, name):
-    """Print formatting for a dictionary."""
-    out = "%s:\n\n" % (name.upper())
-    for key in dictionary:
-        out += (str(key)+': '+', '.join(dictionary[key])+'\n')
-    return out
-
-def print_page(q_id='Q178051'):
-    try:
-        item = pywikibot.ItemPage(REPO, str(q_id))
-        print(item.exists())
-        item_dict = item.get() #Get the item dictionary
-        if item.claims:
-            if 'P31' in item.claims: # instance of
-                print(item.claims['P31'][0].getTarget())
-        clm_dict = str(printer(item_dict["aliases"], "aliases"))
-        # clm_dict = str(printer(item_dict["claims"],"claims" ))
-        clm_dict += str([item_dict["aliases"]] + [item_dict["claims"]]) # Get the claim dictionary
-    except:
-        clm_dict = "INVALID QID"
-    return str(clm_dict)
-
-def get_formats_generator():
-    """ Convenience method to return the list of file formats using the SPARQL
-    query file. """
-    query_file_path = get_query_path('list-formats.rq')
-    return get_sparql_generator(query_file_path, SITE)
-
-def get_sparql_generator(query_file_path, site):
-    """ Opens the SPARQL file at query_file_path, reads the contents and queries
-    SITE for the data and returns the generator. """
-    with open(query_file_path, 'r') as query_file:
-        query = query_file.read().replace('\n', '')
-    generator = pg.WikidataQueryPageGenerator(query, site=site)
-    return generator
-
-def get_query_path(query_file_name):
-    """ Munges the path to a query file together """
-    return os.path.join(THIS_DIR, 'queries/' + query_file_name)
+def test_query():
+    login_instance = wdi_login.WDLogin(user=APP.config.WIKIDATA_USER_NAME, pwd=APP.config.WIKIDATA_PASSWORD)
+    results = wdi_core.WDItemEngine.execute_sparql_query(QUERIES['list-formats'].replace('\n', ''))
+    return results
