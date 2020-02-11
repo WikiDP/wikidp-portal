ac9fa1415f9e348c3ef4fc635ecc7148a411bdd1
Carl Wilson <carl@openpreservation.org>
2020-02-11 02:48:29 +0000

    FEAT: Basic user profile page - added skeleton user profile page wired into OAuth; - removed redundant output strings for profile output; and - wired user icon to profile page.

--- wikidp/static/js/wikidp.js
+++ wikidp/static/js/wikidp.js
@@ -2,10 +2,9 @@ $(document).ready(() => {
   const $pagecontainer = $('div.page-container')
   $('form#navbarSearch').submit(() => $pagecontainer.fadeOut(500))
   $('div.side-icon-div i#searchToggle').click(toggleSearchForm)
-  $('#loginBtn').click(() => { window.location.href = '/login' })
+  $('#loginBtn').click(() => { window.location.href = '/profile' })
   $pagecontainer.fadeIn(500)
 })
-
 
 function toggleSearchForm () {
   let $icon = $('div.side-icon-div i#searchToggle').fadeOut('slow')
@@ -13,14 +12,12 @@ function toggleSearchForm () {
   $icon.toggleClass('fa-search').toggleClass('fa-times-circle-o').fadeIn('slow')
 }
 
-
 function get_item_summary (qid, callback) {
   $.get(`/api/${qid}/summary`, (item) => {
     if (callback) return callback(item)
     return item
   }).fail((error) => console.log(error))
 }
-
 
 function selector_to_data_array (selector) {
   const output = []
